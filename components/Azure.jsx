d1005abfb665f4b57ba0883f81f5739a810aa990
aryanvichare <aryanvichare10@gmail.com>
2021-05-21 15:50:21 -0700

    Add landing page

---
+++ components/Azure.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+
+const Azure = () => {
+  return (
+    <section className='relative w-full px-8 font-sans leading-6 bg-primary border-0 border-gray-200 border-solid text-gray-50'>
+      <div className='flex flex-col items-center max-w-6xl px-8 py-20 mx-auto leading-6 border-solid md:items-stretch md:justify-center md:py-24'>
+        <h2 className='w-full m-0 font-sans text-4xl font-black tracking-wide text-center border-0 border-gray-200 sm:text-5xl text-gray-50'>
+          ⚡️ Supercharged with Azure AI
+        </h2>
+        <p className='w-full max-w-3xl mx-auto mt-4 mb-0 font-sans text-sm font-medium leading-relaxed text-center text-gray-200 border-0 border-gray-200 lg:text-lg md:text-base'>
+          Explore how we leveraged Microsoft Azure AI technologies to{" "}
+          <i>supercharge</i> our mobile application.
+        </p>
+        <div className='grid grid-cols-12 items-center justify-center mx-auto'>
+          <div className='max-w-xs col-span-6 font-sans border-0 border-gray-200 text-gray-50'>
+            <div className='box-border flex flex-col items-center h-full px-2 py-8 mx-4 leading-6 text-center border-solid sm:items-center sm:text-left'>
+              <span className='flex-shrink-0 p-5 font-sans border-0 border-gray-200 rounded-full bg-gray-50 text-gray-50'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='w-8 h-8 text-primary'
+                  width='44'
+                  height='44'
+                  viewBox='0 0 24 24'
+                  stroke-width='1.5'
+                  stroke='currentColor'
+                  fill='none'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'>
+                  <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                  <path d='M10 12h-1v5h1' />
+                  <path d='M14 12h1v5h-1' />
+                  <path d='M14 3v4a1 1 0 0 0 1 1h4' />
+                  <path d='M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z' />
+                </svg>
+              </span>
+              <div className='mt-6 font-sans text-center border-0 border-gray-200 sm:text-center text-gray-50'>
+                <span className='box-border text-xl font-bold leading-none tracking-wider text-center border-solid sm:text-center text-gray-50'>
+                  Azure TTS
+                </span>
+                <p className='box-border mx-0 mt-2 mb-0 font-normal leading-snug text-center text-white border-solid sm:text-center'>
+                  For accessibility and those who have reading barriers
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='max-w-xs col-span-6 font-sans border-0 border-gray-200 text-gray-50'>
+            <div className='box-border flex flex-col items-center h-full px-2 py-8 mx-4 leading-6 text-center border-solid sm:items-center sm:text-left'>
+              <span className='flex-shrink-0 p-5 font-sans border-0 border-gray-200 rounded-full bg-gray-50 text-gray-50'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='w-8 h-8 text-primary'
+                  width='44'
+                  height='44'
+                  viewBox='0 0 24 24'
+                  stroke-width='1.5'
+                  stroke='currentColor'
+                  fill='none'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'>
+                  <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                  <path d='M10 12h-1v5h1' />
+                  <path d='M14 12h1v5h-1' />
+                  <path d='M14 3v4a1 1 0 0 0 1 1h4' />
+                  <path d='M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z' />
+                </svg>
+              </span>
+              <div className='mt-6 font-sans text-center border-0 border-gray-200 sm:text-center text-gray-50'>
+                <span className='box-border text-xl font-bold leading-none tracking-wider text-center border-solid sm:text-center text-gray-50'>
+                  Azure Translate API
+                </span>
+                <p className='box-border mx-0 mt-2 mb-0 font-normal leading-snug text-center text-white border-solid sm:text-center'>
+                  Translation to match the refugee's native language
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='max-w-xs col-span-6 font-sans border-0 border-gray-200 text-gray-50'>
+            <div className='box-border flex flex-col items-center h-full px-2 py-8 mx-4 leading-6 text-center border-solid sm:items-center sm:text-left'>
+              <span className='flex-shrink-0 p-5 font-sans border-0 border-gray-200 rounded-full bg-gray-50 text-gray-50'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='w-8 h-8 text-primary'
+                  width='44'
+                  height='44'
+                  viewBox='0 0 24 24'
+                  stroke-width='1.5'
+                  stroke='currentColor'
+                  fill='none'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'>
+                  <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                  <path d='M10 12h-1v5h1' />
+                  <path d='M14 12h1v5h-1' />
+                  <path d='M14 3v4a1 1 0 0 0 1 1h4' />
+                  <path d='M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z' />
+                </svg>
+              </span>
+              <div className='mt-6 font-sans text-center border-0 border-gray-200 sm:text-center text-gray-50'>
+                <span className='box-border text-xl font-bold leading-none tracking-wider text-center border-solid sm:text-center text-gray-50'>
+                  Azure Speech to Text
+                </span>
+                <p className='box-border mx-0 mt-2 mb-0 font-normal leading-snug text-center text-white border-solid sm:text-center'>
+                  For accessibility and other stuff... add here stuff
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='max-w-xs col-span-6 font-sans border-0 border-gray-200 text-gray-50'>
+            <div className='box-border flex flex-col items-center h-full px-2 py-8 mx-4 leading-6 text-center border-solid sm:items-center sm:text-left'>
+              <span className='flex-shrink-0 p-5 font-sans border-0 border-gray-200 rounded-full bg-gray-50 text-gray-50'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='w-8 h-8 text-primary'
+                  width='44'
+                  height='44'
+                  viewBox='0 0 24 24'
+                  stroke-width='1.5'
+                  stroke='currentColor'
+                  fill='none'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'>
+                  <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                  <path d='M10 12h-1v5h1' />
+                  <path d='M14 12h1v5h-1' />
+                  <path d='M14 3v4a1 1 0 0 0 1 1h4' />
+                  <path d='M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z' />
+                </svg>
+              </span>
+              <div className='mt-6 font-sans text-center border-0 border-gray-200 sm:text-center text-gray-50'>
+                <span className='box-border text-xl font-bold leading-none tracking-wider text-center border-solid sm:text-center text-gray-50'>
+                  Azure Computer Vision
+                </span>
+                <p className='box-border mx-0 mt-2 mb-0 font-normal leading-snug text-center text-white border-solid sm:text-center'>
+                  Computer Vision and OCR Technologies to manage documents
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Azure;
---
+++ components/Landing.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import SocialSignIn from "./SocialSignIn";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const Landing = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <React.Fragment>
+      <section className='relative w-full bg-hero-pattern'>
+        <div className='absolute inset-0 w-full h-full bg-gray-900 opacity-50' />
+        <nav className='max-w-6xl mx-auto relative flex items-center justify-between w-full h-20 px-8'>
+          <img
+            className='w-8'
+            src='images/gradient-logo.png'
+            alt='Sharan Logo'
+          />
+          <div className='flex flex-row justify-center items-center space-x-8'>
+            <a
+              href='#'
+              className='relative text-lg font-medium tracking-wide transition duration-150 ease-out md:text-sm text-white hover:text-indigo-200'>
+              <span className='block'>About</span>
+            </a>
+            <a
+              href='#'
+              className='relative text-lg font-medium tracking-wide transition duration-150 ease-out md:text-sm text-white hover:text-indigo-200'>
+              <span className='block'>Download</span>
+            </a>
+          </div>
+        </nav>
+        <div className='relative z-10 max-w-6xl px-10 py-40 mx-auto'>
+          <div className='flex flex-col items-center h-full lg:flex-row'>
+            <div className='flex flex-col items-center justify-center w-full h-full lg:w-2/3 lg:items-start'>
+              <p className='inline-block w-auto px-3 py-1 mb-5 text-xs font-medium text-white uppercase bg-indigo-600 rounded-full bg-gradient-to-br'>
+                New features released
+              </p>
+              <h1 className='font-extrabold tracking-tight text-center text-white text-4xl md:text-7xl lg:text-left xl:pr-32'>
+                Helping refugees{" "}
+                <span className='text-indigo-200'>resettle.</span>
+              </h1>
+              <p className='font-poppins text-white font-bold text-md lg:text-lg mt-8 text-center lg:text-left'>
+                Using artificial intelligence, Sharan helps refugees plan out a
+                future away from their past, simplifying processes such as
+                registering, finding places to live, jobs managing identity
+                cards and more.
+              </p>
+              <div className='flex flex-row items-center justify-start mt-6'>
+                <Link
+                  className='item-link'
+                  activeClass='active'
+                  to='features'
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}>
+                  <a
+                    target='_blank'
+                    className='font-poppins cursor-pointer flex items-center justify-center w-full pl-4 pr-6 py-3 mb-3 text-lg text-white bg-indigo-600 hover:bg-indigo-500 rounded-md sm:mb-0 transition-colors duration-150 sm:w-auto'>
+                    <svg
+                      className='h-6 w-6 text-white mr-1'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'>
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth={2}
+                        d='M9 5l7 7-7 7'
+                      />
+                    </svg>
+                    Learn More
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        {open && <SocialSignIn setOpen={setOpen} />}
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
