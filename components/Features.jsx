fa64c67f6e119ef4a5930598bfb0302fecba0cb0
aryanvichare <aryanvichare10@gmail.com>
2021-05-22 01:58:42 -0700

    add new features

--- components/Features.jsx
+++ components/Features.jsx
@@ -9,46 +9,61 @@ const Features = () => {
             Features
           </h2>
           <p className='font-poppins w-full py-8 mx-auto -mt-2 text-lg text-center text-gray-700 intro sm:max-w-3xl'>
-            Add some nice touches to your interface with our latest designs,
-            components, and templates. We've crafted a beautiful user experience
-            that your visitors will love.{" "}
+            Explore the features we packed into Sharan to provide relief
+            services to refugees
           </p>
         </div>
         <div className='flex flex-col mb-8 animated fadeIn sm:flex-row'>
           <div className='flex items-center mb-8 sm:w-1/2 md:w-5/12'>
-            <img src='images/feature-2.png' alt='Feature 2' />
+            <img src='images/feature-1.png' alt='Feature 1' />
           </div>
           <div className='flex flex-col justify-center mt-5 mb-8 md:mt-0 sm:w-1/2 md:w-7/12 sm:pl-16'>
             <p className='mb-2 text-sm font-semibold leading-none text-left text-primary uppercase'>
-              know your data
+              Finding Matches
             </p>
             <h3 className='mt-2 text-2xl sm:text-left md:text-4xl'>
-              Real-time Vitals & Alerts
+              Find Jobs & Shelters
             </h3>
             <p className='font-poppins mt-5 text-lg text-gray-700 text md:text-left'>
-              Access your heart rate, SpO<sub>2</sub> levels, body temperature,
-              galvanic skin response and more with our dashboard. Monitor your
-              activity, such as steps taken or calories burnt, and also
-              atmospheric details like ambient humidity, air pressure, etc.
-              <br /> <br />
-              Get notified of any personal/general health emergencies, potential
-              contact with COVID patients or if you enter a containment zone.
+              Based on your profile – skillset, socioeconomic background, etc –
+              we match relevant jobs and shelters that meet your needs.
             </p>
           </div>
         </div>
         <div className='flex flex-col mb-8 animated fadeIn sm:flex-row'>
           <div className='flex items-center mb-8 sm:w-1/2 md:w-5/12 sm:order-last'>
-            <img src='images/feature-3.png' alt='Feature 3' />
+            <img src='images/feature-2.png' alt='Feature 2' />
           </div>
           <div className='flex flex-col justify-center mt-5 mb-8 md:mt-0 sm:w-1/2 md:w-7/12 sm:pr-16'>
             <p className='mb-2 text-sm font-semibold leading-none text-left text-primary uppercase'>
-              Safety is Priority
+              User Privacy
             </p>
-            <h3 className='mt-2 text-2xl sm:text-left md:text-4xl'>......</h3>
+            <h3 className='mt-2 text-2xl sm:text-left md:text-4xl'>
+              Facial Recognition
+            </h3>
             <p className='font-poppins mt-5 text-lg text-gray-700 text md:text-left'>
-              Check the concentration of COVID-19 cases in your area, and if
-              you’ve visited any of the containment zones. This data is taken
-              from your Google Maps Timeline.
+              Sharan uses Facial Recognition technology to securely authenticate
+              the refugees. Using this methodology enables Sharan to maintain
+              user privacy and anonymity protocols.
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col mb-8 animated fadeIn sm:flex-row'>
+          <div className='flex items-center mb-8 sm:w-1/2 md:w-5/12'>
+            <img src='images/feature-3-new.png' alt='Feature 3' />
+          </div>
+          <div className='flex flex-col justify-center mt-5 mb-8 md:mt-0 sm:w-1/2 md:w-7/12 sm:pl-16'>
+            <p className='mb-2 text-sm font-semibold leading-none text-left text-primary uppercase'>
+              Data Matching
+            </p>
+            <h3 className='mt-2 text-2xl sm:text-left md:text-4xl'>
+              Refugee Data
+            </h3>
+            <p className='font-poppins mt-5 text-lg text-gray-700 text md:text-left'>
+              Sharan heavily uses your data to create the most relevant and
+              personalized experience. Not only do was ask for personal details,
+              but we also ask for your financial background to tailor potential
+              jobs and shelters most appropriate to your needs.
             </p>
           </div>
         </div>
